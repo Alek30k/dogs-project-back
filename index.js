9c63c2e771801213acccf2d46b5ddb5bc73a1b0e
Alek30k <alemank23k@gmail.com>
2022-12-11 20:46:57 -0300

    last commit

--- index.js
+++ index.js
@@ -17,14 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { PORT } = process.env;
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 8800;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
-
